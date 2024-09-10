14c8adbb241d16a58d7da9a72c8511fabb7f794c
lehai4 <86140878+lehai4@users.noreply.github.com>
2024-09-10 14:02:37 +0700

    upload

---
+++ src/App.tsx
@@ -0,0 +1,188 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [profile, setProfile] = useState<any>();
+  const [matKhau, setMatKhau] = useState<string>("");
+  const [matKhauNew, setMatKhauNew] = useState<string>("");
+  const [maNV, setMaNV] = useState<string>("");
+
+  const [res, setRes] = useState<any>(null);
+
+  const [isUpdated, setIsUpdated] = useState<boolean>(false);
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+    const data = { MaNV: maNV, MatKhau: matKhau };
+    try {
+      const response = await fetch("http://localhost:3000/v1/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        mode: "cors",
+        body: JSON.stringify(data),
+        credentials: "include",
+      });
+      const result = await response.json();
+      setMaNV("");
+      setMatKhau("");
+      setRes(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleUpdate = async (e: any) => {
+    const data = { currentPassword: matKhau, newPassword: matKhauNew };
+
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:3000/v1/auth/update-password",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          mode: "cors",
+          body: JSON.stringify(data),
+          credentials: "include",
+        }
+      );
+      const result = await response.json();
+      setMatKhau("");
+      setMatKhauNew("");
+      setRes(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleProfile = async () => {
+    const res = await fetch("http://localhost:3000/v1/auth/profile", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      mode: "cors",
+      credentials: "include",
+    });
+    const profile = await res.json();
+    setProfile(profile);
+  };
+
+  const LogOut = async () => {
+    const res = await fetch("http://localhost:3000/v1/auth/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+    const result = await res.json();
+    setRes(result);
+  };
+
+  return (
+    <div className="card">
+      <form onSubmit={!isUpdated ? handleLogin : handleUpdate}>
+        {!isUpdated ? (
+          <>
+            <h5>Đăng Nhập</h5>
+            <div className="form-control" id="">
+              <input
+                value={maNV}
+                placeholder="MaNV..."
+                onChange={(e) => setMaNV(e.target.value)}
+              />
+            </div>
+            <div className="form-control" id="">
+              <input
+                value={matKhau}
+                placeholder="MatKhau..."
+                onChange={(e) => setMatKhau(e.target.value)}
+              />
+            </div>
+            <button type="submit" style={{ margin: "20px 0" }}>
+              Login
+            </button>
+          </>
+        ) : (
+          <>
+            <h5>Cập nhật</h5>
+            <div className="form-control" id="">
+              <input
+                value={matKhau}
+                placeholder="MatKhau..."
+                onChange={(e) => setMatKhau(e.target.value)}
+              />
+            </div>
+            <div className="form-control" id="">
+              <input
+                value={matKhauNew}
+                placeholder="MatKhau Moi..."
+                onChange={(e) => setMatKhauNew(e.target.value)}
+              />
+            </div>
+            <button type="submit" style={{ margin: "20px 0" }}>
+              Update
+            </button>
+          </>
+        )}
+      </form>
+      <div style={{ display: "flex", flexDirection: "row", columnGap: "24px" }}>
+        <button
+          style={{ margin: "20px 0" }}
+          onClick={() => setIsUpdated(false)}
+        >
+          Login
+        </button>
+
+        <button style={{ margin: "20px 0" }} onClick={() => setIsUpdated(true)}>
+          Update Password
+        </button>
+        <button
+          className="logout"
+          style={{ margin: "20px 0" }}
+          onClick={handleProfile}
+        >
+          Profile
+        </button>
+        <button
+          className="logout"
+          style={{ margin: "20px 0" }}
+          onClick={LogOut}
+        >
+          LOGOUT
+        </button>
+      </div>
+
+      {res && <p>Thông báo: {res.message}</p>}
+
+      {profile?.success === true ? (
+        <>
+          <p className="notification" style={{ color: "blue" }}>
+            {profile.message}
+          </p>
+          <h4>Thông Tin Nhân Viên</h4>
+          <div
+            className="profile"
+            style={{
+              margin: "20px 0",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div>MaNV:{profile.nhanVien.MaNV}</div>
+            <div>MaNV:{profile.nhanVien.TenNV}</div>
+            <div>CCCD:{profile.nhanVien.CCCD}</div>
+            <div>Điện thoại:{profile.nhanVien.DienThoai}</div>
+            <div>Công Việc Phụ Trách:{profile.nhanVien.CongViecPhuTrach}</div>
+            <div>
+              Trạng thái hoạt động:
+              {!profile.nhanVien.IsActive ? "Hoạt động" : "Đang nghỉ việc"}
+            </div>
+          </div>
+        </>
+      ) : (
+        <h3 style={{ color: "red" }}>Lỗi profile:{profile?.message}</h3>
+      )}
+    </div>
+  );
+}
+
+export default App;
